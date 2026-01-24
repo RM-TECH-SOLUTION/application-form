8870cc4f47416265ca89a94ccc5bfa2ebb002250
RamMohanRao <rammohan@froogal.ai>
2026-01-24 18:06:43 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import ValentineForm from "@/components/ValentineForm";
-import PaymentSummary from "@/components/PaymentSummary";
 
 export default function HomePage() {
   return <ValentineForm />;
